646a8ba6b90093c2d0782bbaae70f97151bcb324
Du Hyun Cho <33812854+duhyuncho1994@users.noreply.github.com>
2025-05-11 13:39:55 +0200

    Update main2.java

--- src/main/java/com/example/main2.java
+++ src/main/java/com/example/main2.java
@@ -14,7 +14,7 @@ import net.automatalib.automaton.fsa.impl.CompactDFA;
 import net.automatalib.util.automaton.builder.AutomatonBuilders;
 import java.util.*;
 
-public class main2 {
+public class Main {
 
     private static CompactDFA<Character> constructSUL() {
         Alphabet<Character> sigma = Alphabets.characters('0', '1');
